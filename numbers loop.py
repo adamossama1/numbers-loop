bf4b0f869f2659c0ae4f87d9b17bb4848aee766e
adamossama1 <adamossama357@gmail.com>
2022-08-18 09:16:46 +0200

    python projects

---
+++ numbers loop.py
@@ -0,0 +1,21 @@
+#you can choose any way from these 2 ways
+'''
+number=int(input('enter the number:'))
+
+x=0
+while x<=number:
+    print(x)
+    x+=1
+    
+print('done')
+    
+'''
+
+
+number=int(input('enter the number:'))
+
+for number in range(1,number+1):
+    print(number)
+
+
+print('done')
